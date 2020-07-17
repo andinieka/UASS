92291f38a595812f66912fe04fbd7dea9f1ee9e6
andinieka <62147591+andinieka@users.noreply.github.com>
2020-07-17 21:07:38 +0700

    push file index
    andini eka putri

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+    $host     = "localhost";
+    $username = "id14360605_uasweb2";
+    $password = "w|DZPvW<k7nFF}w[";
+    $database = "id14360605_uas";
+    $conn  = mysqli_connect($host, $username, $password, $database);
+
+    if (!$conn) {
+        die("Koneksi Gagal" . mysqli_connect_error());
+    } else {
+    	
+    }
+?>
